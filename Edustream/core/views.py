7167c8a2047e01b180e0d306048ccf3e47b2e1dd
Vinay akula <144476612+vinayakula06@users.noreply.github.com>
2025-07-21 16:54:22 +0530

    Update views.py

--- Edustream/core/views.py
+++ Edustream/core/views.py
@@ -1,5 +1,4 @@
 # core/views.py
-
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
