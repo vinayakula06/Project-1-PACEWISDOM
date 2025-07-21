be5deede3f7241badfdc5937c6cb479868b37e6e
Vinay akula <144476612+vinayakula06@users.noreply.github.com>
2025-07-21 16:52:57 +0530

    Update urls.py

--- Edustream/core/urls.py
+++ Edustream/core/urls.py
@@ -1,8 +1,6 @@
 # core/urls.py
-
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
