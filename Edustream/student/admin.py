63c6eeac89ea3f40208418f949493bb89007428e
Vinay akula <144476612+vinayakula06@users.noreply.github.com>
2025-07-21 16:55:45 +0530

    Update admin.py

--- Edustream/student/admin.py
+++ Edustream/student/admin.py
@@ -1,5 +1,2 @@
 # student/admin.py
-
-# This app does not require its own admin registrations if models are defined in 'core'
-# and registered there. This file can be empty.
 from django.contrib import admin
